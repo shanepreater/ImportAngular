4ebdd4e5fac1251cb8a98c4ed06df11e9785cd4b
Shane Preater <shane.preater@gmail.com>
2014-09-07 18:09:52 +0100

    Added the initial rule editor work

--- importer-web/src/main/webapp/js/rules.js
+++ importer-web/src/main/webapp/js/rules.js
@@ -1,10 +1,60 @@
-function RuleEditor() {
-	var $hostElement = ${"div class=\"ruleEditor\"></div>"};
-	
-	var constructDialog = function() {
-		$hostElement.load("fragments/ruleEditor.html");
-		
-		//Hook up the angular js links.
-		
+(function() {
+	var rulesHelper = {
+			currentId : 2,
+			nextId : function() {
+				this.currentId = this.currentId + 1;
+				return this.currentId;
+			}
 	};
-}
+	
+	var rulesModule = angular.module("rules", []);
+	
+	rulesModule.controller("RuleEditorController", ["$http", "$scope", "$timeout", "$log", function($http, $scope, $timeout, $log){
+		var scopedThis = this;
+		this.showDialog = function(rule, readOnly) {
+			if(rule) {
+				scopedThis.rule = rule;
+			} else {
+				scopedThis.rule = {id : "new"};
+			}
+			if(readOnly) {
+				scopedThis.readOnly = true;
+			} else {
+				scopedThis.readOnly = false;
+			}
+			
+			$("ruleEditorModal").modal();
+		};
+		
+		this.isFieldReadOnly = function(fieldName) {
+			if(scopedThis.readOnly) {
+				return true;
+			}
+			
+			if(scopedThis.rule.id !== "new") {
+				if(fieldName === "name") {
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		this.saveRule = function() {
+			//TODO: Post this to the server.
+			if(scopedThis.rule.id === "new") {
+				scopedThis.rule.id = rulesHelper.nextId();
+			}
+			
+			/*
+			$http.post("api/rule", scopedThis.rule).success(function(data){
+				scopedThis.rule = data;
+				$timeout(function(){
+					$("ruleEditorModal").modal("hide");
+				}, 1000);
+			});*/
+			
+			$scope.$broadcast("ruleSaved", scopedThis.rule);
+		};
+	}]);
+})();
